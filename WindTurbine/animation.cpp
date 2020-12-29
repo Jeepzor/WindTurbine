cc937dc6bab7e322d96b99c85ccd51062f58a0b8
Simon <simonrahnasto@gmail.com>
2020-12-29 17:40:39 +0100

    Clean Animation -> Remove unne
    Remove unnecessary loc. initializing member variables which already are initialized by the parent.

--- WindTurbine/animation.cpp
+++ WindTurbine/animation.cpp
@@ -1,16 +1,7 @@
 #include "animation.h"
 
-//Constructor for animated images: Move to own class, Animation, which inherits Image.
-
 namespace wind {
 	Animation::Animation(std::string path, int amout_of_frames, double animation_rate) : wind::Image::Image(path) {
-		surface = IMG_Load(path.c_str());
-
-		width = surface->w;
-		height = surface->h;
-		angle = 0;
-
-		//Animation
 		timer = 0;
 		rate = animation_rate;
 		frames = amout_of_frames;
@@ -20,8 +11,6 @@ namespace wind {
 		portion = { 0,0, frameWidth,height };
 		destination = { 0, 0, frameWidth, height };
 		originPoint = { frameWidth / 2, height / 2 };
-		asset = SDL_CreateTextureFromSurface(wind::turbine.getRenderer(), surface);
-		SDL_FreeSurface(surface);
 	}
 
 	SDL_Rect* Animation::getPortion() {
