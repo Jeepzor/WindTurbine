b8122de2770790ea2581050e358676648c2114c0
Simon <simonrahnasto@gmail.com>
2021-01-12 17:52:49 +0100

    Animation - Documentation + Security

--- WindTurbine/animation.h
+++ WindTurbine/animation.h
@@ -9,14 +9,26 @@ namespace wind {
 	class Animation : public Drawable {
 	public:
 		~Animation()override;
-		void update(double dt);
-		void incrementFrame();
 
+		/// <summary>
+		/// Updates the animation, looping through it's frames.
+		/// </summary>
+		/// <param name="dt"> = Delta Time</param>
+		void update(double dt);
+
+		/// <summary>
+		/// Used to instantiate new Animations.
+		/// </summary>
+		/// <param name="path"> = Path to the image file</param>
+		/// <param name="amount_of_frames"> = Number of frames in the animation</param>
+		/// <param name="animation_rate"> = Time (in milliseconds) between frames</param>
+		/// <returns>Pointer to an Animation</returns>
 		static Animation* getInstance(std::string path, int amount_of_frames, double animation_rate) {
 			return new Animation(path, amount_of_frames, animation_rate);
 		}
 
 	private:
+		void incrementFrame();
 		Animation(std::string path, int amout_of_frames, double animation_rate);
 		Animation(const Animation& other) = delete;
 		const Animation& operator=(const Animation& other) = delete;
