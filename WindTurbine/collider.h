7e0fb7218c387a04c627808c8cd09bb0343adb2c
Simon <simonrahnasto@gmail.com>
2021-01-12 12:02:24 +0100

    Add documentation to Collider

--- WindTurbine/collider.h
+++ WindTurbine/collider.h
@@ -11,22 +11,83 @@ namespace wind {
 	class Collider{
 	public:
 		virtual ~Collider()= 0 {};
+
+		/// <summary>
+		/// Updates the collider.
+		/// </summary>
+		/// <param name="dt"> = Delta Time</param>
 		virtual void update(double dt);
+
+		/// <summary>
+		/// Draws the collider
+		/// </summary>
 		virtual void draw() const = 0;
-		virtual void move();
+
+		/// <summary>
+		/// Set the velocity of the collider, in pixels per second.
+		/// </summary>
+		/// <param name="x"> = X-velocity</param>
+		/// <param name="y"> = Y-velocity</param>
 		virtual void setVelocity(double x, double y);
-		virtual void rotateVertices(double dt) {};
+
+		/// <summary>
+		/// Sets the rotation of the collider (Only affects Polygons).
+		/// </summary>
+		/// <param name="amount"></param>
 		virtual void setRotation(double amount) {};
+
+		/// <summary>
+		/// Get the X position of the collider.
+		/// </summary>
+		/// <returns>double X</returns>
 		double getX() const;
+
+		/// <summary>
+		/// Get the Y position of the collider.
+		/// </summary>
+		/// <returns>double Y</returns>
 		double getY() const;
+
+		/// <summary>
+		/// Get X position of the center of the collider.
+		/// </summary>
+		/// <returns>double X</returns>
 		double getCenterX() const;
+
+		/// <summary>
+		/// Get Y position of the center of the collider.
+		/// </summary>
+		/// <returns>double Y</returns>
 		double getCenterY() const;
+
+		/// <summary>
+		/// Get the theoretical next position of the collider on the X-axis.
+		/// </summary>
+		/// <returns>double X</returns>
 		double getNextX() const;
+
+		/// <summary>
+		/// Get the theoretical next position of the collider on the Y-axis.
+		/// </summary>
+		/// <returns>double Y</returns>
 		double getNextY() const;
+
+		/// <summary>
+		/// Return the radius of the shape, every shape has a radius in order to do a less costly collision detection algorithm first, before checking more exact and expensive ones.
+		/// </summary>
+		/// <returns>double X</returns>
 		int getRadius() const;
+
+		/// <summary>
+		/// Gets the shape of the collider.
+		/// </summary>
+		/// <returns>Enum Shape</returns>
 		Shape getShape() const;
 		
 	protected:
+		virtual void move();
+		virtual void rotateVertices(double dt) {};
+
 		Collider(PhysicsWorld* world, double x, double y, int radius); 
 		Collider(const Collider& other) = delete;
 		const Collider& operator=(const Collider& other) = delete;
